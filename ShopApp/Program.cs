8bad770d13eb7fd1cd7f745d95cfa34360d85eee
cengiztankas <cengizasd.3810@gmail.com>
2023-01-02 18:49:56 +0300

    ShopApp insert

---
+++ ShopApp/Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("merhaba");
+            // InsertOneProduct();
+            InsertListProduct();
+            Console.WriteLine("kayıt başarılı");
+        }
+        static void InsertOneProduct()  
+        {
+            
+            using(var db=new ShopContext()){
+                var product1= new Product{ Name="Samsung S5",Price=5000,CategoryId= 1 };
+                db.Products.Add(product1);
+                db.SaveChanges();
+
+            }
+
+        }
+        static void InsertListProduct(){
+
+            var Productslist=new List<Product>(){
+             new Product() {Name="Samsung S6",Price=6000,CategoryId=1},
+             new Product(){Name="Samsung S7",Price=7000,CategoryId=1 },
+             new Product() {Name="Samsung S8", Price=8000,CategoryId=1}
+            };
+           
+
+            using(var db=new ShopContext()){
+                db.AddRange(Productslist);
+                db.SaveChanges();
+            }
+        }
+
+    }
+   
+     public class ShopContext: DbContext
+    {  
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+     
+         protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source=ShopApp.db");
+            
+     }
+
+ public class Product
+    { 
+        public int Id { get; set; }
+       
+        [MaxLength(100)]
+        [Required]
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+
+    }
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+   
